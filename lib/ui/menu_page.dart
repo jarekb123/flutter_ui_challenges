0d18e1411b0eb5e952dd2d37c90c28a2fae00c99
Jarosław Butajło <jaroslaw.butajlo@allbright.io>
2021-04-18 21:11:12 +0200

    implement example of horizontal ListView in scrollable Column

--- lib/ui/menu_page.dart
+++ lib/ui/menu_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_challenges/ui/scrollable/footer_in_scrollable_example.dart';
+import 'package:flutter_ui_challenges/ui/scrollable/horizontal_listview_in_scrollable_column.dart';
 
 class MenuPage extends StatelessWidget {
   const MenuPage({Key? key}) : super(key: key);
@@ -22,7 +23,17 @@ class MenuPage extends StatelessWidget {
                 ),
               ),
               child: Text('Footer in Scrollable Column'),
-            )
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => HorizontalListViewInScrollable(),
+                ),
+              ),
+              child: Text('Horizontal ListView in Scrollable Column'),
+            ),
           ],
         ),
       ),
---
+++ lib/ui/scrollable/horizontal_listview_in_scrollable_column.dart
@@ -0,0 +1,109 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+const _colors = [
+  Colors.red,
+  Colors.blue,
+  Colors.yellow,
+  Colors.green,
+  Colors.orange,
+  Colors.purple,
+  Colors.black,
+];
+
+class HorizontalListViewInScrollable extends StatefulWidget {
+  const HorizontalListViewInScrollable({Key? key}) : super(key: key);
+
+  @override
+  _HorizontalListViewInScrollableState createState() =>
+      _HorizontalListViewInScrollableState();
+}
+
+class _HorizontalListViewInScrollableState
+    extends State<HorizontalListViewInScrollable> {
+  double? _listViewHeight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Horizontal ListView in Scrollable Column'),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Text(
+              'It is possible to have horizontal ListView in scrollable Column. But it is needed to calculate size of widget with the largest height.',
+            ),
+            SizedBox(height: 16),
+            if (_listViewHeight == null)
+              OffstageWidgetSizeProvider(
+                onSizeCalculated: (size) =>
+                    setState(() => _listViewHeight = size.height),
+                child: _Item(),
+              ),
+            if (_listViewHeight != null)
+              SizedBox(
+                height: _listViewHeight,
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) => _Item(),
+                  separatorBuilder: (_, __) => SizedBox(width: 8),
+                  itemCount: 100,
+                ),
+              ),
+            SizedBox(height: 16),
+            ..._colors.map(
+              (color) => Container(
+                height: 50,
+                width: double.infinity,
+                color: color,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class OffstageWidgetSizeProvider extends StatelessWidget {
+  const OffstageWidgetSizeProvider({
+    Key? key,
+    required this.onSizeCalculated,
+    required this.child,
+  }) : super(key: key);
+
+  final ValueChanged<Size> onSizeCalculated;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Offstage(
+      child: Builder(
+        builder: (context) {
+          WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+            if (context.size != null) onSizeCalculated(context.size!);
+          });
+
+          return child;
+        },
+      ),
+    );
+  }
+}
+
+class _Item extends StatelessWidget {
+  const _Item({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: _colors[Random().nextInt(_colors.length)],
+      height: 300,
+      width: 200,
+    );
+  }
+}
